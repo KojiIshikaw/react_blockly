d8637c8784c4c2e977c86b36a5fdcd9c925f8c95
KojiIshikaw <koji.ishikawa@techshare.co.jp>
2025-01-12 00:04:53 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,47 @@
 // App.tsx
 import React, { useEffect } from 'react';
-import * as Blockly from 'blockly/core';
+import * as Blockly from 'blockly';
 import { javascriptGenerator } from 'blockly/javascript';
 import { addCustomBlocks } from './customBlocks';
 
 const App: React.FC = () => {
 	useEffect(() => {
+		// カスタムブロックを初期化
 		addCustomBlocks();
+
+		// モダンな形式でツールボックスを定義
+		const toolboxConfiguration = {
+			kind: 'flyoutToolbox',
+			contents: [
+				{
+					kind: 'block',
+					type: 'custom_block'
+				}
+			]
+		};
 
 		// Blocklyワークスペースの初期化
 		const workspace = Blockly.inject('blocklyDiv', {
-			toolbox: `
-                <xml xmlns="https://developers.google.com/blockly/xml">
-                    <block type="custom_block"></block>
-                </xml>
-            `,
+			toolbox: toolboxConfiguration
 		});
 
-		// 必要に応じてJavaScriptコードを生成
-		const code = javascriptGenerator.workspaceToCode(workspace);
-		console.log(code);
+		// クリーンアップ関数
+		return () => {
+			workspace.dispose();
+		};
 	}, []);
 
 	return (
 		<div>
 			<h1>Blockly with React and Vite</h1>
-			<div id="blocklyDiv" style={{ height: '480px', width: '600px' }}></div>
+			<div
+				id="blocklyDiv"
+				style={{
+					height: '480px',
+					width: '600px',
+					border: '1px solid #ccc'  // 視覚的なフィードバック用
+				}}
+			/>
 		</div>
 	);
 };
